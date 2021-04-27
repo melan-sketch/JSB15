a8d18e2163f4055033d4e64e797ac0ed850ca8f3
Phuong Lan Nguyen <80739332+melan-sketch@users.noreply.github.com>
2021-04-27 23:44:56 +0700

    Delete review directory

--- review/code.js
+++
@@ -1,7 +0,0 @@
-let page = document.getElementById('submit')
-page.addEventListener('submit', changeinnerHTML,)
-
-function changeinnerHTML() {
-    document.body.style.innerHTML = 'User have already logged in...'
-}
-
